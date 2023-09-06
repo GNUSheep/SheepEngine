6f38154ca42879bfc4c322bcdeb828c54881395b
GNUSheep <pentet221@gmail.com>
2023-09-06 23:25:09 +0200

    TODO: write own chess ui, for the temp, chessboard.js and chess.js are used to handle legal moves, and chess ui

---
+++ chess.js
@@ -0,0 +1,39 @@
+var game = new Chess()
+
+function onDragStart(square, piece) {
+    var moves = game.moves({
+        square: square,
+        verbose: true
+    })
+    
+    moves.forEach(square => {
+        var square_obj = $('#chess_board .square-' + square.to)
+        square_obj.addClass('highlight1-32417')
+    //    square.classList.add('highlight1-32417')
+    });
+}
+
+function onDrop(source, target) {
+    var move = game.move({
+        from: source,
+        to: target,
+        promotion: 'q'
+    })
+    
+      if (move === null) return 'snapback'
+}
+
+function onSnapEnd () {
+    chess_board.position(game.fen())
+}
+
+var config = {
+    draggable: true,
+    onDragStart: onDragStart,
+    onDrop: onDrop,
+    onSnapEnd: onSnapEnd,
+    position: 'start',
+}
+
+var chess_board = Chessboard('chess_board', config)
+$(window).resize(chess_board.resize)
--- js/globals.js
+++
@@ -1,9 +0,0 @@
-const pieces = {
-    p: "<div class=\"piece\"><i class=\"fa-solid fa-chess-pawn\"></i></div>",
-    r: "<div class=\"piece\"><i class=\"fa-solid fa-chess-rook\"></i></div>",
-    n: "<div class=\"piece\"><i class=\"fa-solid fa-chess-knight\"></i></div>",
-    b: "<div class=\"piece\"><i class=\"fa-solid fa-chess-bishop\"></i></div>",
-    k: "<div class=\"piece\"><i class=\"fa-solid fa-chess-king\"></i></div>",
-    q: "<div class=\"piece\"><i class=\"fa-solid fa-chess-queen\"></i></div>",
-};
-const chess_board = document.getElementById("chess_board");
