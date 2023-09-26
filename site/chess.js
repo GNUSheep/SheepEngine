43424d7d42e7a1f878b8795a464ac875b8e5f503
GNUSheep <pentet221@gmail.com>
2023-09-26 15:20:20 +0200

    Add piece tables, better evaluation

--- site/chess.js
+++ site/chess.js
@@ -1,4 +1,6 @@
-var game = new Chess('1K6/6r1/1k6/8/8/8/8/3R4 w - - 0 1')
+//var game = new Chess('1nbqk2r/6pp/8/r7/3p4/3p2P1/5PKP/4q3 w k - 1 32')
+// 8/3r4/1p2r2p/1k4pK/7P/4b3/6P1/8 b - - 0 54
+var game = new Chess()
 
 function onDragStart(square, piece) {
     var moves = game.moves({
@@ -16,6 +18,7 @@ function onDrop(source, target) {
     var move = game.move({
         from: source,
         to: target,
+        promotion: 'q',
     })
     
     if (move === null) {    
@@ -25,11 +28,12 @@ function onDrop(source, target) {
     $.post("http://localhost:8080", game.fen(), (data, status) => {
         console.log(data)
         let from = data.slice(0, 2)
-        let to = data.slice(2)
+        let to = data.slice(2, 4)
 
         game.move({
             from: from,
             to: to,
+            promotion: 'q',
         })
         chess_board.position(game.fen())
     });
@@ -44,7 +48,8 @@ var config = {
     onDragStart: onDragStart,
     onDrop: onDrop,
     onSnapEnd: onSnapEnd,
-    position: '1K6/6r1/1k6/8/8/8/8/3R4',
+    //position: '1nbqk2r/6pp/8/r7/3p4/3p2P1/5PKP/4q3',
+    position: 'start',
 }
 
 var chess_board = Chessboard('chess_board', config)
--- src/engine.rs
+++ src/engine.rs
@@ -1,7 +1,62 @@
 use chess::{Board, MoveGen, Color, BoardStatus, Game, GameResult};
 use std::str::FromStr;
 
-static CHESS_PIECES_VALUE: [i32; 5] = [100, 300, 300, 500, 900];
+static CHESS_PIECES_VALUE: [i32; 5] = [100, 320, 330, 500, 900];
+
+static PAWN_TABLE: [i32; 64] = [
+    0,  0,  0,  0,  0,  0,  0,  0,
+    50, 50, 50, 50, 50, 50, 50, 50,
+    10, 10, 20, 30, 30, 20, 10, 10,
+    5,  5, 10, 25, 25, 10,  5,  5,
+    0,  0,  0, 20, 20,  0,  0,  0,
+    5, -5,-10,  0,  0,-10, -5,  5,
+    5, 10, 10,-20,-20, 10, 10,  5,
+    0,  0,  0,  0,  0,  0,  0,  0
+];
+
+static KNIGHT_TABLE: [i32; 64] = [
+    -50,-40,-30,-30,-30,-30,-40,-50,
+    -40,-20,  0,  0,  0,  0,-20,-40,
+    -30,  0, 10, 15, 15, 10,  0,-30,
+    -30,  5, 15, 20, 20, 15,  5,-30,
+    -30,  0, 15, 20, 20, 15,  0,-30,
+    -30,  5, 10, 15, 15, 10,  5,-30,
+    -40,-20,  0,  5,  5,  0,-20,-40,
+    -50,-40,-30,-30,-30,-30,-40,-50,
+];
+
+static BISHOP_TABLE: [i32; 64] = [
+    -20,-10,-10,-10,-10,-10,-10,-20,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -10,  0,  5, 10, 10,  5,  0,-10,
+    -10,  5,  5, 10, 10,  5,  5,-10,
+    -10,  0, 10, 10, 10, 10,  0,-10,
+    -10, 10, 10, 10, 10, 10, 10,-10,
+    -10,  5,  0,  0,  0,  0,  5,-10,
+    -20,-10,-10,-10,-10,-10,-10,-20,
+];
+
+static ROOK_TABLE: [i32; 64] = [
+    0,  0,  0,  0,  0,  0,  0,  0,
+    5, 10, 10, 10, 10, 10, 10,  5,
+   -5,  0,  0,  0,  0,  0,  0, -5,
+   -5,  0,  0,  0,  0,  0,  0, -5,
+   -5,  0,  0,  0,  0,  0,  0, -5,
+   -5,  0,  0,  0,  0,  0,  0, -5,
+   -5,  0,  0,  0,  0,  0,  0, -5,
+    0,  0,  0,  5,  5,  0,  0,  0
+];
+
+static QUEEN_TABLE: [i32; 64] = [
+    -20,-10,-10, -5, -5,-10,-10,-20,
+    -10,  0,  0,  0,  0,  0,  0,-10,
+    -10,  0,  5,  5,  5,  5,  0,-10,
+     -5,  0,  5,  5,  5,  5,  0, -5,
+      0,  0,  5,  5,  5,  5,  0, -5,
+    -10,  5,  5,  5,  5,  5,  0,-10,
+    -10,  0,  5,  0,  0,  0,  0,-10,
+    -20,-10,-10, -5, -5,-10,-10,-20
+];
 
 fn checkmate_check(mut alpha: i32, beta: i32, board: Board, fen: &str) -> i32 {
     let evaluation = evaluate_position(fen, board);
@@ -33,8 +88,8 @@ fn checkmate_check(mut alpha: i32, beta: i32, board: Board, fen: &str) -> i32 {
 
 fn search(depth: i32, fen: &str, board: Board, mut alpha: i32, beta: i32) -> (i32, String) {
     if depth == 0 {
-        //return (checkmate_check(alpha, beta, board, fen), "".to_string())
-        return (evaluate_position(fen, board), "".to_string())
+        return (checkmate_check(alpha, beta, board, fen), "".to_string())
+        //return (evaluate_position(fen, board), "".to_string())
     }
 
     let moves = MoveGen::new_legal(&board);
@@ -43,26 +98,26 @@ fn search(depth: i32, fen: &str, board: Board, mut alpha: i32, beta: i32) -> (i3
         let game: Game = Game::from_str(fen).expect("Valid FEN");
         let status = game.result().unwrap();
 //
-    //    if status == GameResult::WhiteCheckmates {
-    //        return (-900000000, "".to_string())
-    //    }
+        if status == GameResult::WhiteCheckmates {
+            return (-900000000, "".to_string())
+        }
 //
         if status == GameResult::BlackCheckmates {
             return (2000000000, "checkmate".to_string())
         }
 //
-    //    return (0, "".to_string())
+        return (0, "".to_string())
     }
 
     let mut best_move = "".to_string();
 
     for move_element in moves {
         let new_board = board.make_move_new(move_element);
-        //println!("{}, {}, {}, {}", move_element.to_string(), alpha, beta, new_board.to_string());
+        println!("{}, {}, {}, {}, {}", move_element.to_string(), alpha, beta, best_move, new_board.to_string());
         let (out, check_move) = search(depth - 1, new_board.to_string().as_str(), new_board, -beta, -alpha);
 
         if check_move == "checkmate" {
-            println!("{}", move_element.to_string());
+            //println!("{}", move_element.to_string());
             alpha = -out;
             best_move = move_element.to_string();
             return (alpha, best_move)
@@ -73,6 +128,7 @@ fn search(depth: i32, fen: &str, board: Board, mut alpha: i32, beta: i32) -> (i3
         }
 
         if alpha < -out {
+        //    println!("{}", -out);
             alpha = -out;
             best_move = move_element.to_string();
         }
@@ -86,41 +142,54 @@ fn evaluate_position(fen: &str, board: Board) -> i32 {
 
     let mut black_evaluation = 0;
     let mut white_evaluation = 0;
+
+    let mut square_index = 0;
     for row in fen_splited.iter() {
         for piece in row.chars() {
             match piece {
                 'p' => {
-                    black_evaluation += CHESS_PIECES_VALUE[0];           
+                    black_evaluation += CHESS_PIECES_VALUE[0];
+                    black_evaluation += PAWN_TABLE.iter().copied().rev().collect::<Vec<_>>()[square_index];      
                 },
                 'P' => {
-                    white_evaluation += CHESS_PIECES_VALUE[0];  
+                    white_evaluation += CHESS_PIECES_VALUE[0];
+                    white_evaluation += PAWN_TABLE[square_index];    
                 }
                 'n' => {
-                    black_evaluation += CHESS_PIECES_VALUE[1];           
+                    black_evaluation += CHESS_PIECES_VALUE[1]; 
+                    black_evaluation += KNIGHT_TABLE.iter().copied().rev().collect::<Vec<_>>()[square_index];      
                 },
                 'N' => {
-                    white_evaluation += CHESS_PIECES_VALUE[1];  
+                    white_evaluation += CHESS_PIECES_VALUE[1];
+                    white_evaluation += KNIGHT_TABLE[square_index]; 
                 }
                 'b' => {
-                    black_evaluation += CHESS_PIECES_VALUE[2];           
+                    black_evaluation += CHESS_PIECES_VALUE[2];
+                    black_evaluation += BISHOP_TABLE.iter().copied().rev().collect::<Vec<_>>()[square_index];         
                 },
                 'B' => {
-                    white_evaluation += CHESS_PIECES_VALUE[2];  
+                    white_evaluation += CHESS_PIECES_VALUE[2];
+                    white_evaluation += BISHOP_TABLE[square_index]; 
                 }
                 'r' => {
-                    black_evaluation += CHESS_PIECES_VALUE[3];           
+                    black_evaluation += CHESS_PIECES_VALUE[3];
+                    black_evaluation += ROOK_TABLE.iter().copied().rev().collect::<Vec<_>>()[square_index];           
                 },
                 'R' => {
-                    white_evaluation += CHESS_PIECES_VALUE[3];  
+                    white_evaluation += CHESS_PIECES_VALUE[3];
+                    white_evaluation += ROOK_TABLE[square_index]; 
                 }
                 'q' => {
-                    black_evaluation += CHESS_PIECES_VALUE[4];           
+                    black_evaluation += CHESS_PIECES_VALUE[4];
+                    black_evaluation += QUEEN_TABLE.iter().copied().rev().collect::<Vec<_>>()[square_index];           
                 },
                 'Q' => {
                     white_evaluation += CHESS_PIECES_VALUE[4];  
+                    white_evaluation += QUEEN_TABLE[square_index]; 
                 }
                 _ => (),
             }
+            square_index += 1;
         }
     }
 
@@ -128,7 +197,11 @@ fn evaluate_position(fen: &str, board: Board) -> i32 {
     let status = game.result();
     if status != None {
         if status.unwrap() == GameResult::BlackCheckmates {
-            black_evaluation = 100000000;
+            black_evaluation = 200000000;
+        }
+
+        if status.unwrap() == GameResult::WhiteCheckmates {
+            white_evaluation = 200000000;
         }
     }
 
@@ -141,7 +214,7 @@ fn evaluate_position(fen: &str, board: Board) -> i32 {
 
 pub fn make_move(fen: &str) -> String {
     let chess_board = Board::from_str(fen).unwrap();
-
+//"1nbqk2r/6pp/8/r7/3p4/3p1KP1/5P1P/4q3 b k - 1 32" 
     let (out, best_move) = search(2, fen, chess_board, -100000000, 100000000);
     println!("Best: {}, {}", out, best_move);
 
